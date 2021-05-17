d5e5a517f60028fcbec8c356c1797ab0613242e4
0925Daiki <82555115+0925Daiki@users.noreply.github.com>
2021-05-17 12:30:51 +0900

    トップページ実装・devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :teacher_name, presence: true
+  validates :school_name, presence: true
+end
